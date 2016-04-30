448199d83b96979ce24da16a420cad8828593b24
leo81202 <nucklesix@gmail.com>
2016-04-30 01:48:54 -0400

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+var BUILD_DIR = path.resolve(__dirname, 'public/');
+var APP_DIR = path.resolve(__dirname, 'src/');
+
+var config = {
+  entry: APP_DIR + '/scripts/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?/,
+      include: APP_DIR,
+      loader: 'babel'
+    }, {
+      test: /\.scss$/,
+      loaders: ["style", "css", "sass"]
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      },
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+
+  ]
+};
+
+module.exports = config;
